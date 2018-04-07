0e755114f8d1e07d79b86155a71bdb2c378c6980
Rick Henry <fredericmhenry@gmail.com>
2018-04-07 14:15:34 -0400

    basic functions written
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

---
+++ getfiles.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def download_list():
+    page = requests.get("https://github.com/fish-shell/fish-shell/tree/master/share/completions")
+    soup = BeautifulSoup(page.text, 'html.parser')
+    files = soup.find(class_='file-wrap')
+    each_file = files.find_all('a', 'js-navigation-open')
+    return each_file[1:]
+
+
+def download_file(file_tags):
+    file_url = "https://github.com" + file_tags.replace('blob', 'raw')
+    file_data = requests.get(file_url)
+    with open(file_tags.text, 'w') as thefile:
+        thefile.write(file_data.text)
